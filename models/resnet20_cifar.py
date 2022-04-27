86e7a4fe5b2f08486c5b40fba67aa3c61b9a7f2e
junghunoh <dh6dh@snu.ac.kr>
2022-04-27 21:22:25 +0900

    preact_resnet

--- models/resnet20_cifar.py
+++ models/resnet20_cifar.py
@@ -8,6 +8,7 @@ def conv3x3(in_planes, out_planes, stride=1):
     return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
                      padding=1, bias=False)
 
+''' for norelu_after_resconnection.pth
 class BasicBlock(nn.Module):
     expansion = 1
 
@@ -42,6 +43,41 @@ class BasicBlock(nn.Module):
         #out = self.relu(out)
 
         return out
+'''
+
+#preact_resnet
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None, last=False):
+        super(BasicBlock, self).__init__()
+        self.conv1 = conv3x3(inplanes, planes, stride)
+        self.bn1 = nn.BatchNorm2d(inplanes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.downsample = downsample
+        self.stride = stride
+        self.last = last
+
+    def forward(self, x):
+        residual = x
+        
+        out = self.bn1(x)
+        out = self.relu(out)
+        out = self.conv1(out)
+
+        out = self.bn2(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+
+        return out
+
 
 class ResNet(nn.Module):
 
